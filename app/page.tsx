fe2769f4e511f75fe2b7647fbeae5da61f4fb447
Richard Branson <238357@gmail.com>
2025-02-14 16:39:48 +0800

    first link

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
+              <a href="https://v2ex.com/t/1111276#reply2">	用 Cursor 做的在线计算器网站，欢迎提建议</a>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
