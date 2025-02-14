16f2911c03054c112753d1b6910b6032f55d987b
Richard Branson <238357@gmail.com>
2025-02-14 16:44:39 +0800

    title why this

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <h1>创业者HUB</h1>
+        <p>收集来自YC,V2EX的好玩的新网站</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
               <a href="https://v2ex.com/t/1111276#reply2">	用 Cursor 做的在线计算器网站，欢迎提建议</a>
